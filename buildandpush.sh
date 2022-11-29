12a4f67d12051a856840bae9d5616e35d7a781c2
unknown <juanca.fernandez18@gmail.com>
2022-11-28 21:42:23 -0300

    registry

--- buildandpush.sh
+++ buildandpush.sh
@@ -5,13 +5,13 @@ filename="version"
 
 while read -r line; do
    echo "$line"
-    docker tag backend $CI_REGISTRY/juanca.fernandez18/tpdevops2022:backend${line}
-    docker tag backend $CI_REGISTRY/juanca.fernandez18//tpdevops2022:frontend${line}
-    docker tag backend $CI_REGISTRY/juanca.fernandez18/tpdevops2022:mysql-db${line}
+    docker tag backend registry.gitlab.com/juanca.fernandez18/tpdevops2022:backend${line}
+    docker tag backend registry.gitlab.com/juanca.fernandez18/tpdevops2022:frontend${line}
+    docker tag backend registry.gitlab.com/juanca.fernandez18/tpdevops2022:mysql-db${line}
 
-    docker push $CI_REGISTRY/juanca.fernandez18//tpdevops2022:backend${line}
-    docker push $CI_REGISTRY/juanca.fernandez18//tpdevops2022:frontend${line}        
-    docker push $CI_REGISTRY/juanca.fernandez18//tpdevops2022:mysql-db${line}       
+    docker push registry.gitlab.com/juanca.fernandez18/tpdevops2022:backend${line}
+    docker push registry.gitlab.com/juanca.fernandez18/tpdevops2022:frontend${line}        
+    docker push registry.gitlab.com/juanca.fernandez18/tpdevops2022:mysql-db${line}       
 echo "FIN DEL SCRIPT"
 done < "$filename"
 
